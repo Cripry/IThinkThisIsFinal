d1461c7ea458f6ef953384ba6d2a50c20c629f3f
Cristian Preguza <cristian.preguza@gmail.com>
2023-09-30 19:42:55 +0300

    first commit

---
+++ apps/wind-turbine/src/pages/dashboard.tsx
@@ -0,0 +1,13 @@
+// apps/wind-turbine/src/pages/dashboard.tsx
+import * as React from 'react';
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            {/* Rest of your Dashboard page content */}
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ apps/wind-turbine/src/pages/data.tsx
@@ -0,0 +1,13 @@
+// apps/wind-turbine/src/pages/data.tsx
+import * as React from 'react';
+
+function Data() {
+    return (
+        <div>
+            <h1>Data</h1>
+            {/* Rest of your Data page content */}
+        </div>
+    );
+}
+
+export default Data;
